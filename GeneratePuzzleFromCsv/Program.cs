918bcbda7fd43c9251a74b334ddb15ca39a8bfdc
abdul baruwa <abdulbaruwa@hotmail.com>
2012-10-31 05:59:56 +0000

    Creating Simple Data Generator

---
+++ GeneratePuzzleFromCsv/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using LinqToExcel;
+using LinqToExcel.Domain;
+
+namespace GeneratePuzzleFromCsv
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string path = @"c:\documents";
+            //Open CSV file
+            var excel = new ExcelQueryFactory(@"C:\Development\OnGitHub\CrossSharp\GeneratePuzzleFromCsv\PuzzleGroup");
+            excel.DatabaseEngine = DatabaseEngine.Jet;
+            var puzzleGroups = from c in excel.Worksheet<PuzzleGroup>("PuzzleGroup")
+                       select new PuzzleGroup(){PuzzleGroupId = c.PuzzleGroupId, Name = c.Name};
+
+            
+
+            //var puzzleSubGroups = from c in excel.Worksheet<PuzzleSubGroup>("PuzzleSubGroup")
+            //                      select new PuzzleSubGroup() {Title = c.Title, PuzzleGroupId = };
+
+    
+            //var puzzleGame = from c in excel.Worksheet<PuzzleGame>("PuzzleGame")
+            //                 select new PuzzleGame() {PuzzleGameId = c.PuzzleGameId};
+
+
+            //foreach (var puzzleSubGroup in puzzleSubGroups)
+            //{
+            //    puzzleSubGroup.PuzzleGames.AddRange(puzzleGame.Where(x => x.));
+            //}
+
+
+
+        }
+    }
+}
---
+++ GeneratePuzzleFromCsv/PuzzleGroup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace GeneratePuzzleFromCsv
+{
+    //Science
+    public class PuzzleGroup
+    {
+        public int PuzzleGroupId { get; set; }
+        public string Name { get; set; }
+        public List<PuzzleSubGroup> Puzzles { get; set; }
+    }
+
+    //KS2 - Science
+    public class PuzzleSubGroup
+    {
+        public string Title { get; set; }
+        public int PuzzleGroupId { get; set; }
+        public List<PuzzleGame> PuzzleGames { get; set; }
+    }
+
+    public class PuzzleGame
+    {
+        public int PuzzleGameId { get; set; }
+        public List<string> Words { get; set; }
+    }
+
+    public class PuzzleGroupData
+    {
+        public int PuzzleGroupDataId { get; set; }
+        public string Data { get; set; }
+    }
+}
